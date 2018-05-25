7c43140a44d0c0fe4f2ee54f9e3c1a7206fc423c
Leonardo Luiz Silotti <32528580+silotti@users.noreply.github.com>
2018-05-25 20:03:47 -0300

    Add files via upload

---
+++ ConsoleApplicationAlunos/Alunos/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno
+{
+    class Aluno
+    {
+        public String nome { get; set; }
+        public DateTime nascimento { get; set; }
+        public String matricula { get; set; }
+    }
+}
---
+++ ConsoleApplicationAlunos/Alunos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> alunos = new List<Aluno>();
+            int total = 10;
+            Random r = new Random();
+            DateTime dataInicial = new DateTime(1990, 01, 01);
+            DateTime dataFinal = DateTime.Now.AddYears(-16);
+            TimeSpan tempo = (dataFinal - dataInicial);
+            while (total-->0)
+            {
+                Aluno aluno = new Aluno();
+                aluno.nome = "Nome " + r.Next(100);
+                aluno.matricula=r.Next (500).ToString();
+                aluno.nascimento = dataInicial.AddDays(r.Next((int)tempo.TotalDays));
+                alunos.Add(aluno);
+            }
+
+            var maioresDe18 =
+                from a in alunos
+                where (DateTime.Now - a.nascimento).TotalDays > (365 * 18)
+                select a;
+
+            foreach (Aluno m in maioresDe18)
+            {
+                Console.WriteLine("Alunos maiores de 18: "+m.nome);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
